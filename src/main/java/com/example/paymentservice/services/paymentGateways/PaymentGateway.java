4141209cabc50d7ce146e4ce5565138fc8a97f7b
Akshay Kumar <akshaykumarvk@outlook.com>
2024-06-08 12:47:03 +0530

    first stripe commit

--- src/main/java/com/example/paymentservice/services/paymentGateways/PaymentGateway.java
+++ src/main/java/com/example/paymentservice/services/paymentGateways/PaymentGateway.java
@@ -1,9 +1,8 @@
 package com.example.paymentservice.services.paymentGateways;
 
 import com.stripe.exception.StripeException;
-import org.springframework.beans.factory.annotation.Qualifier;
 
 public interface PaymentGateway {
 
-   public String  generatePaymentLink(String orderId, Long amount) throws StripeException;
+   String generatePaymentLink (String orderId, Long amount) throws StripeException;
 }
